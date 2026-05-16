5a032141f32aedd3ee4bd6c6484d2bf800fa9673
Bruce Taylor <bdtsat@gmail.com>
2026-05-16 10:10:18 -0700

    Revert Cell Phone formatting

--- lib/build_info.dart
+++ lib/build_info.dart
@@ -1,4 +1,4 @@
 // AUTO-GENERATED — do not edit. Re-generated on each build by scripts/gen_build_info.sh
-const String kBuildHash = 'c4d83ae';
-const String kBuildDate = '2026-05-15 19:52 UTC';
-const int kBuildNumber = 25;
+const String kBuildHash = '4f1c23d';
+const String kBuildDate = '2026-05-16 17:07 UTC';
+const int kBuildNumber = 28;
