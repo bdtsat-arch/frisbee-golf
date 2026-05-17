69d1897172eb288fc300c81e682f00806a97dc2d
Bruce Taylor <bdtsat@gmail.com>
2026-05-17 13:26:15 -0700

    Ver 38 - Adding Cloud Storage for Photos

--- lib/build_info.dart
+++ lib/build_info.dart
@@ -1,4 +1,4 @@
 // AUTO-GENERATED — do not edit. Re-generated on each build by scripts/gen_build_info.sh
-const String kBuildHash = '0160bfb';
-const String kBuildDate = '2026-05-17 05:35 UTC';
-const int kBuildNumber = 36;
+const String kBuildHash = '2659b22';
+const String kBuildDate = '2026-05-17 20:25 UTC';
+const int kBuildNumber = 38;
